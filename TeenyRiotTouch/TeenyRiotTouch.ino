037722e1efa2c4afe01298d54bdbe3f313a8e64b
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2016-04-04 14:26:25 +0200

    preset serial and midi

--- TeenyRiotTouch/TeenyRiotTouch.ino
+++ TeenyRiotTouch/TeenyRiotTouch.ino
@@ -1,13 +1,18 @@
-#include "ArduinoMain.h"
+#define USE_MIDI
 
-//#include <TeenyMouse.h>
+#ifdef USE_MIDI
+#include <TeenyMidi.h>
+#else
 #include <TeenySerial.h>
+#endif
 
-#include "TeenyTouchDusjagr.h"
 #include <TeenyTouch.h>
 
+#include "TeenyTouchDusjagr.h"
+
 TeenyTouch test;
-//TeenyTouchDusjagr test2;
+
+  //TeenyTouchDusjagr test2;
 // ATMEL ATTINY85
 //
 //                   +-\/-+
@@ -20,18 +25,50 @@ TeenyTouch test;
 // * indicates PWM port
 //
 
+int prevValue = 0;
+int velocityValue = 0;
+
 void setup()
 {
-    test.begin(PB4,PB2);
-    //TeenyTouchDusjagr.begin();
-    //TeenyTouchDusjagr.begin();
+  //test.begin(PB4,PB2);
+  #ifdef USE_MIDI
+    TeenyMidi.init();
+  #else
     TeenySerial.begin();
-    // TeenyMouse.begin(); //start or reenumerate USB - BREAKING CHANGE from old versions that didn't require this
+  #endif
+  TeenyTouchDusjagr.begin();
 }
+
+#ifdef USE_MIDI
+void sendCC(uint8_t ch, uint8_t value)
+{
+    TeenyMidi.send(MIDI_CONTROLCHANGE, ch, value);
+}
+
+void sendHiresMidi(uint16_t value)
+{
+    uint8_t valH = value >> 3;
+    uint8_t valL = value & 0b0111;
+    TeenyMidi.send(MIDI_CONTROLCHANGE, 1, valH);
+    TeenyMidi.send(MIDI_CONTROLCHANGE, 2, valL);
+}
+#endif
 
 void loop()
 {
-    TeenySerial.println(test.getRawValue(10));
-    //TeenySerial.println(TeenyTouchDusjagr.sense(PB4,PB2, 10));
+  int value = TeenyTouchDusjagr.sense(PB4,PB2, 100);
+  velocityValue = value-prevValue;
+
+  #ifdef USE_MIDI
+
+  sendHiresMidi(value);
+  TeenyMidi.delay(1); // give some time for sending, otherwhise the MIDI queue could fill up
+
+  #else
+    TeenySerial.println(velocityValue); //wrap your strings in F() to save ram!
     TeenySerial.delay(1);
+  #endif
+
+  prevValue = value;
+
 }
