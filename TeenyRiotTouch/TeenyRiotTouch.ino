e6c6a3fb3eec45ac9bd20b27e98997ac52a8db6a
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2016-04-09 17:28:15 +0200

    add 2 sensor multiplexer

--- TeenyRiotTouch/TeenyRiotTouch.ino
+++ TeenyRiotTouch/TeenyRiotTouch.ino
@@ -9,7 +9,7 @@
 //#include <TeenyTouch.h>
 
 #include "TeenyTouchDusjagr.h"
-//#include "AnalogTouch.h"
+#include "AnalogTouch.h"
 
 MIDIMessage midimsg;
 //TeenyTouch test;
@@ -29,14 +29,19 @@ MIDIMessage midimsg;
 
 int i = -1023;
 int value = 0;
+int value2 = 0;
 int prevValue = 0;
 int velocityValue = 0;
+
+int prevValue2 = 0;
+int velocityValue2 = 0;
 
 uint8_t samples;
 uint8_t midi_delay;
 uint8_t sense_mode;
 uint8_t send_mode;
 uint8_t multiplex_ch;
+uint8_t multiplex_ch2;
 
 uint16_t timer = 0;
 
@@ -96,6 +101,8 @@ void setup()
     send_mode = 0;
     samples = 100;
     multiplex_ch = 0;
+    multiplex_ch = 0;
+
     setAnalogMultiplexCh(multiplex_ch);
     pinMode(PB0, OUTPUT);
 
@@ -104,7 +111,7 @@ void setup()
 
 void loop()
 {
-    setAnalogMultiplexCh(multiplex_ch);
+
 
     switch (sense_mode) {
     case 1:
@@ -114,20 +121,29 @@ void loop()
         value = TeenyTouchDusjagr.touchPin(PB4,samples);
         break;
     case 3:
+        setAnalogMultiplexCh(multiplex_ch);
         value = TeenyTouchDusjagr.sense(PB4,PB3, samples);
+        setAnalogMultiplexCh(multiplex_ch2);
+        value2 = TeenyTouchDusjagr.sense(PB4,PB3, samples);
+
         break;
     case 4:
-        value = analogRead(2);
+        value = analogRead(3);
         break;
     case 5:
         i++;
         if (i> 1023) i = -1023;
         value = i;
         break;
+    case 6:
+        value = analogTouchRead(2, samples);
     }
 
     velocityValue = value-prevValue;
     prevValue = value;
+
+    velocityValue2 = value2-prevValue2;
+    prevValue2 = value2;
 
 #ifdef USE_MIDI
 
@@ -174,14 +190,22 @@ void loop()
            multiplex_ch = midimsg.value;
        }
 
+       if (midimsg.key == 9) //midi delay
+       {
+           multiplex_ch2 = midimsg.value;
+       }
+
    }
 
 
    if (send_mode == 1)
    {
         TeenyMidi.sendCCHires(velocityValue, 1);
+        TeenyMidi.sendCCHires(velocityValue2, 5);
+
     }else{
         TeenyMidi.sendCCHires(value, 1);
+        TeenyMidi.sendCCHires(value2, 5);
    }
 
     TeenyMidi.delay(midi_delay); // give some time for sending, otherwhise the MIDI queue could fill up
